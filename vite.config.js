7704e17d2b246c5e26fe660f70fd4673a5bda0ae
Aitsada <aitsada.a1@gmail.com>
2026-06-05 16:47:26 +0700

    fix: mode is not defined

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   server: {
     host: "127.0.0.1",
   },
   base: mode === "github" ? "/Escaping-Game-PJ/" : "/",
-});
+}));
